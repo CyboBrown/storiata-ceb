fe07dbdcc5cc1eab246006c7370d6ca28a81f741
Christian Seville <seville2020christian@gmail.com>
2024-04-08 21:06:31 +0800

    Added basic signup, signin, edit profile functionalities
    Also changed project language from javascript to typescript

---
+++ src/lib/supabase.ts
@@ -0,0 +1,19 @@
+import "react-native-url-polyfill/auto";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
+import {
+  REACT_NATIVE_SUPABASE_URL,
+  REACT_NATIVE_SUPABASE_ANON_KEY,
+} from "../../hidden/secrets";
+
+const supabaseUrl = REACT_NATIVE_SUPABASE_URL;
+const supabaseAnonKey = REACT_NATIVE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
