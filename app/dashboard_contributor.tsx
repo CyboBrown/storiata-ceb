e9486d93485d0d124409cad72f6dc80802163d60
Terraknighty <johnjosephpableo@Gmail.com>
2024-05-26 12:25:07 +0800

    Contributor Dashboard

--- app/dashboard_contributor.tsx
+++ app/dashboard_contributor.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import config from "../tamagui.config";
 import { Session } from "@supabase/supabase-js";
-import { YStack, Text, View } from "tamagui";
+import { YStack, Text, View, Input, Card, CardHeader, CardFooter, CardBackground, ScrollView } from "tamagui";
 
 export default function ContributorDashboard({
   session,
@@ -12,19 +12,43 @@ export default function ContributorDashboard({
   useEffect(() => {
     console.log("CONTRIBUTOR_DASHBOARD page loaded.");
   }, []);
+
+  const[search, setSearch] = useState("");
+
   return (
-    <>
-    <View>
-      <Text color="white"> WELCOME TO STORIATA </Text>
+    <ScrollView>
+    <View justifyContent="center" alignItems="center">
+      <Text color="white" fontSize={40}>STORIATA</Text>
     </View>
-      <YStack f={1} jc="center" ai="center" backgroundColor={"$backgroundSoft"}>
-        {/* <Paragraph color="$color" jc="center">
-        Dashboard
-      </Paragraph>
-      <H5> </H5>
-      <Button size="$4">Useless Button</Button> */}
-      </YStack>
-      <Text color="white"> Welcome Contributor</Text>
-    </>
+    <View marginStart="$5" marginTop="$3" marginEnd="$5">
+      <Input
+        size="$4"
+        placeholder="search contribution"
+        value={search}
+        onChangeText={(text) => setSearch(text)}/>
+    </View>
+    <View marginStart="$2" marginTop="$3" marginEnd="$2">
+      <Text color="white" fontSize={25}>  Featured Contributions</Text>
+    </View>
+    <View marginStart="$5" marginTop="$3" marginEnd="$5">
+      <Card elevate size={4} bordered height={150}>
+        <CardHeader/>
+        <CardFooter/>
+        {}
+        <CardBackground/>
+      </Card>
+    </View>
+    <View marginStart="$2" marginTop="$5" marginEnd="$2">
+      <Text color="white" fontSize={25}>  Recent Contributions</Text>
+    </View>
+    <View marginStart="$5" marginTop="$3" marginEnd="$5">
+      <Card elevate size={4} bordered height={150}>
+        <CardHeader/>
+        <CardFooter/>
+        {}
+        <CardBackground/>
+      </Card>
+    </View>
+    </ScrollView>
   );
 }
