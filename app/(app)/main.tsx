3f884092e8f3ea033a8de3e249e9ad2f9c93bb71
ShySpaceSheep <theminecraftf303@gmail.com>
2024-10-12 04:42:19 +0800

    Change icon library for bottom nav from `Ionicons` to `MaterialCommunityIcons`

--- app/(app)/main.tsx
+++ app/(app)/main.tsx
@@ -1,19 +1,13 @@
-import type { TabsContentProps } from "tamagui";
-import { ScrollView, Separator, SizableText, Tabs } from "tamagui";
 import Dashboard from "./dashboard";
-import ContributorDashbaord from "./dashboard_contributor";
 import Exercises from "./exercises";
 import Dictionary from "./dictionary";
 import Account from "./account";
-import HeaderTitle from "../../src/components/HeaderTitle";
 import NavAvatarIcon from "../../src/components/NavAvatarIcon";
-import { StatusBar } from "expo-status-bar";
-import { useEffect, useState } from "react";
 import { useSession } from "../../src/services/auth-context";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { StyleSheet, SafeAreaView, useColorScheme } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import Icon from "react-native-vector-icons/Ionicons";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import ContributorDashboard from "./dashboard_contributor";
 
 export default function Main({contribMode}: {contribMode: boolean;}) {
@@ -24,32 +18,31 @@ export default function Main({contribMode}: {contribMode: boolean;}) {
   return (
     <>
       <NavigationContainer independent={true}>
-        <BtmTabNavigator.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
+      <BtmTabNavigator.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
 
-              if (route.name === "Home") {
-                iconName = focused ? "home" : "home-outline";
-              } else if (route.name === "Exercises") {
-                iconName = focused ? "reader" : "reader-outline";
-              } else if (route.name === "Dictionary") {
-                iconName = focused ? "book" : "book-outline";
-              } else if (route.name === "My Account") {
-                return (
-                  <NavAvatarIcon userID={getUserUUID()} size={size} />
-                );
-              }
+            if (route.name === "Home") {
+              iconName = focused ? "home-circle" : "home-circle-outline";
+            } else if (route.name === "Exercises") {
+              iconName = focused ? "bullseye-arrow" : "bullseye";
+            } else if (route.name === "Dictionary") {
+              iconName = focused ? "book-open-page-variant" : "book-open-page-variant-outline";
+            } else if (route.name === "My Account") {
+              return <NavAvatarIcon userID={getUserUUID()} size={size} />;
+            }
 
-              // You can return any component that you like here!
-              return <Icon name={iconName} size={size} color={color} />;
-            },
-            swipeEnabled: true,
-            tabBarActiveTintColor: "dodgerblue",
-            tabBarInactiveTintColor: "gray",
-            headerShown: false,
-          })}
-        >
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "dodgerblue",
+          tabBarInactiveTintColor: "gray",
+          headerShown: true,
+          headerStyle: { backgroundColor: 'dodgerblue' },
+          headerTintColor: 'white',
+          headerTitleStyle: { fontWeight: 'bold' },
+        })}
+      >
 
           {contribMode ? (
             <BtmTabNavigator.Screen
