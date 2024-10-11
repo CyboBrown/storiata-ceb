1cf9b17794ceea1db0205daf055e466b38718562
ShySpaceSheep <theminecraftf303@gmail.com>
2024-10-12 04:49:23 +0800

    Update account profile page to match latest designs

--- app/(app)/account.tsx
+++ app/(app)/account.tsx
@@ -1,46 +1,64 @@
-import { useState, useEffect } from "react";
-import { supabase } from "../../src/utils/supabase";
-import { Alert } from "react-native";
-import { Button, Input, Text, View } from "tamagui";
-import { Session } from "@supabase/supabase-js";
-import Avatar from "./avatar";
-import { router } from "expo-router";
-import { UserAuthentication } from "../../src/services/UserAuthentication";
+import { useState, useEffect, useLayoutEffect } from "react";
+import { ActivityIndicator, StyleSheet, Alert, View, Text, ScrollView } from "react-native";
+import { useNavigation, useRouter } from "expo-router";
 import { AccountService } from "../../src/services/AccountService";
-import { Profile } from "../../src/models/Profile";
 import { useSession } from "../../src/services/auth-context";
+import AvatarDisplay from "../../src/components/AvatarDisplay";
+import BackgroundCircle from "../../src/components/BackgroundCircle";
+import ProfileInfoCard from "../../src/components/ProfileInfoCard";
+import ProfileInteractableCard from "../../src/components/ProfileInteractableCard";
+import CustomHeader from "../../src/components/HeaderTitle";
+import LogoutModal from "../../src/components/LogoutModal";
 
 export default function Account() {
   const [loading, setLoading] = useState(true);
-  const [username, setUsername] = useState("");
-  const [website, setWebsite] = useState("");
+  const [username, setUsername] = useState("My Account");
+  const [fullname, setFullname] = useState("Cebuano Enthusiast")
+  const [email, setEmail] = useState(" ")
+  const [website, setWebsite] = useState("No website indicated")
+  const [isContributor, setIsContributor] = useState(false);
   const [avatarUrl, setAvatarUrl] = useState("");
-  const { session, getUserUUID } = useSession();
+  const [modalVisible, setModalVisible] = useState(false);
+  const { session, getUserUUID, signOut } = useSession();
 
-  // DO NOT DELETE: FOR TESTING AND INITIALIZATION
-  useEffect(() => {
-    console.log("ACCOUNT page loaded.");
-  }, []);
+  const navigation = useNavigation();
+  const router = useRouter();
 
   useEffect(() => {
-    if (session) showProfile();
+    if (session) loadUserDetails();
   }, [session]);
 
-  async function showProfile() {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: () => <CustomHeader pageTitle={username} />,  // Use Custom Header
+      headerStyle: { backgroundColor: 'dodgerblue' },
+      headerTintColor: 'white',
+    });
+  }, [navigation, username]);
+
+  async function loadUserDetails() {
     try {
       setLoading(true);
-      if (!session) throw new Error("No user on the session!");
+      let userDetails = await AccountService.getProfile(getUserUUID());
+      let email = await AccountService.getUserEmail();
 
-      let data = await AccountService.getProfile(getUserUUID());
-
-      if (data?.avatar_url) {
-        setAvatarUrl(data.avatar_url);
+      if (userDetails?.avatar_url) {
+        setAvatarUrl(userDetails.avatar_url);
       }
-      if (data?.username) {
-        setUsername(data.username);
+      if (userDetails?.username) {
+        setUsername(userDetails.username);
       }
-      if (data?.website) {
-        setWebsite(data.website);
+      if (userDetails?.full_name) {
+        setFullname(userDetails.full_name);
+      }
+      if (userDetails?.is_contributor) {
+        setIsContributor(userDetails.is_contributor);
+      }
+      if (userDetails?.website) {
+        setWebsite(userDetails.website);
+      }
+      if (email) {
+        setEmail(email);
       }
     } catch (error) {
       if (error instanceof Error) {
@@ -51,93 +69,92 @@ export default function Account() {
     }
   }
 
-  async function editProfile({
-    username,
-    website,
-    avatar_url,
-  }: {
-    username: string;
-    website: string;
-    avatar_url: string;
-  }) {
-    try {
-      setLoading(true);
-      if (!session?.user) throw new Error("No user on the session!");
-      const updates: Profile = {
-        id: session?.user.id,
-        username,
-        website,
-        avatar_url,
-        updated_at: new Date(),
-      };
-      await AccountService.updateProfile(updates);
-    } catch (error) {
-      if (error instanceof Error) {
-        Alert.alert(error.message);
-      }
-    } finally {
-      setLoading(false);
-    }
+  if (loading) {
+    return (
+      <>
+      <View style={styles.defaultContainer}>
+        <Text style={styles.loadingText}>Please wait a moment while{"\n"} we prepare things around here...</Text>
+        <ActivityIndicator size="large" color="white" />
+      </View>
+      </>
+    );
   }
 
   return (
-    <View flex={1} padding="$4" marginTop="$10">
-      <View alignItems="center" marginBottom="$5">
-        <Avatar
-          size={200}
-          url={avatarUrl}
-          onUpload={(url: string) => {
-            setAvatarUrl(url);
-            editProfile({ username, website, avatar_url: url });
-          }}
-        />
-      </View>
-      <View>
-        <Text
-          color={"$black"}
-          fontFamily={"$heading"}
-          fontSize={20}
-          marginTop="$2"
-        >
-          {" "}
-          User Details
-        </Text>
-      </View>
-      <View marginTop="$3">
-        <Input placeholder="Email" value={session?.user?.email} disabled />
-      </View>
-      <View marginTop="$3">
-        <Input
-          placeholder="Username"
-          value={username || ""}
-          onChangeText={(text) => setUsername(text)}
-        />
-      </View>
-      <View marginTop="$3">
-        <Input
-          placeholder="Website"
-          value={website || ""}
-          onChangeText={(text) => setWebsite(text)}
-        />
-      </View>
-      <View marginTop="$5">
-        <Button
-          onPress={() =>
-            editProfile({ username, website, avatar_url: avatarUrl })
-          }
-          disabled={loading}
-        >
-          Update
-        </Button>
-      </View>
-      <View marginTop="$3">
-        <Button onPress={async () => await UserAuthentication.signOut()}>
-          Sign Out
-        </Button>
-      </View>
-      <View marginTop="$3">
-        <Button onPress={() => router.push("/settings")}>App Settings</Button>
-      </View>
+    <>
+    {/*Fuck this garbage, this ain't gonna work universally.*/}
+    <BackgroundCircle size={400} color="dodgerblue" top={-230} left ={-180} />
+    <BackgroundCircle size={400} color="dodgerblue" top={-230} left ={140} />
+
+    <View style={{alignItems: "center", justifyContent: "center", marginTop: 10, marginBottom: 12}}>
+        <AvatarDisplay userID={getUserUUID()} size={150} borderRadius={100} />
     </View>
+    <View style={{alignItems: "center", justifyContent: "center"}}>
+      <Text style={{fontSize: 24, fontWeight: "900"}}>{fullname}</Text>
+      <Text style={{fontSize: 15, fontWeight: "500"}}>@{username}</Text>
+    </View>
+
+    <View style={styles.userTypeLabel}>
+      {isContributor? (
+        <Text style={{fontSize: 15, fontWeight: "600", color: "white"}}>
+          CONTRIBUTOR
+        </Text>
+      ) : (
+        <Text style={{fontSize: 15, fontWeight: "600", color: "white"}}>
+          LEARNER
+        </Text>
+      )}
+    </View>
+
+    <ScrollView>
+    <View style={{margin: 10, marginLeft: 15}}>
+      <Text style={{fontWeight: "600", color: "gray"}}>User Details</Text>
+    </View>
+    <View style={{backgroundColor: "white", marginLeft: 15, marginRight: 15, marginBottom: 5, borderRadius: 20}}>
+      <ProfileInfoCard iconName="account" title="Full Name" description={fullname}/>
+      <ProfileInfoCard iconName="at" title="Username" description={username}/>
+      <ProfileInfoCard iconName="email" title="Email" description={email}/>
+      <ProfileInfoCard iconName="web" title="Website" description={website}/>
+      <ProfileInteractableCard iconName="account-cog" title="Change User Details" onPress={() => router.push("/edit_user_details")} />
+    </View>
+
+    <View style={{margin: 10, marginLeft: 15}}>
+      <Text style={{fontWeight: "600", color: "gray"}}>Help and Support</Text>
+    </View>
+    <View style={{backgroundColor: "white", marginLeft: 15, marginRight: 15, marginBottom: 15, borderRadius: 20}}>
+      <ProfileInteractableCard iconName="logout" title="Log Out" onPress={() => setModalVisible(true)}/>
+      <ProfileInteractableCard iconName="application-cog" title="Settings" />
+      <ProfileInfoCard iconName="cellphone" title="App Version" description="Alpha-build v.0.1.0" />
+    </View>
+    </ScrollView>
+
+    <LogoutModal modalVisible={modalVisible} setModalVisible={setModalVisible} handleLogOut={signOut} />
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  defaultContainer: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "dodgerblue",
+    justifyContent: "center",
+  },
+  loadingText: {
+    fontSize: 18,
+    color: 'white',
+    textAlign: "center",
+    marginBottom: 25,
+  },
+  userTypeLabel: {
+    marginTop: "5%",
+    marginBottom: "3%",
+    marginLeft: "30%",
+    marginRight: "30%",
+    padding: 5,
+    borderRadius: 15,
+    alignItems: "center", 
+    justifyContent: "center", 
+    backgroundColor: "dodgerblue"
+  }
+});
