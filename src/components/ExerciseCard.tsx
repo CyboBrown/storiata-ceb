fdb452accfe9e0cebc660bc94e9ed59e28910072
ShySpaceSheep <theminecraftf303@gmail.com>
2024-10-18 09:18:42 +0800

    Add progress indicator to `ExerciseCard`

--- src/components/ExerciseCard.tsx
+++ src/components/ExerciseCard.tsx
@@ -1,3 +1,4 @@
+import { green, redDark } from '@tamagui/themes';
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -6,23 +7,39 @@ import { Progress } from 'tamagui';
 interface ComponentProps {
   title: string;
   subtitle: string;
+  progress: number;
   onPress: any;
 }
 
-export default function ExerciseCard({ title, subtitle, onPress }: ComponentProps) {
+export default function ExerciseCard({ title, subtitle, progress, onPress }: ComponentProps) {
   return (
     <>
-    <TouchableOpacity style={styles.cardContainer} onPress={onPress}>
-      <View style={styles.iconContainer}>
-        <Icon name="book" size={50} color="gray" />
+    <TouchableOpacity onPress={onPress} style={styles.cardContainer}>
+      <View style={styles.detailsContainer}>
+        <View style={styles.iconContainer}>
+          <Icon name="book" size={50} color="gray" />
+        </View>
+
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.subtitle}>{subtitle}</Text>
+        </View>
+
+        <Icon name="chevron-right" size={30} color="gray" style={styles.arrow} />
       </View>
 
-      <View style={styles.rightContainer}>
-        <Text style={styles.title}>{title}</Text>
-        <Text style={styles.subtitle}>{subtitle}</Text>
-      </View>
+      <View style={styles.progressContainer}>
+        <View style={{flex: 0.2, flexDirection: "row", alignItems: "center"}}>
+          <Icon name="star-circle" size={15} color="dodgerblue" />
+          <Text style={{color: "gray", fontWeight: "700"}}> {progress.toString()}/6 </Text>
+        </View>
 
-      <Icon name="chevron-right" size={30} color="gray" style={styles.arrow} />
+        <View style={{flex: 0.8}}>
+          <Progress size="$1.5" value={(100 / 6) * progress} width="100%" alignSelf='center'>
+              <Progress.Indicator backgroundColor={'$blue8'} animation="bouncy" />
+          </Progress>
+        </View>
+      </View>
     </TouchableOpacity>
 
       </>
@@ -34,12 +51,19 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     borderRadius: 10,
     elevation: 2,
-    flexDirection: 'row',
     padding: 6,
     marginBottom: 10,
     marginLeft: "1.25%",
     marginRight: "1.25%",
+  },
+  detailsContainer: {
+    flexDirection: 'row',
     alignItems: 'center',
+  },
+  progressContainer: {
+    padding: 6,
+    flexDirection: 'row',
+    alignItems: "center",
   },
   iconContainer: {
     marginRight: 15,
