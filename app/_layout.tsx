a674fad599e35e133d873ea78dbca1d0b6de9895
ShySpaceSheep <theminecraftf303@gmail.com>
2024-12-08 23:32:42 +0800

    Fix critical issue of app crash when unprovided with `<TamaguiProvider>`

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,20 @@ import { Slot } from "expo-router";
 import { StatusBar } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { SessionProvider } from "../src/contexts/AuthContext";
+import { TamaguiProvider, Theme } from "tamagui";
+import { useColorScheme } from "react-native";
+import config from "../tamagui.config";
 
 export default function Root() {
+  const colorScheme = useColorScheme();
   return (
     <SessionProvider>
-      <StatusBar backgroundColor="dodgerblue" />
-      <Slot />
+      <TamaguiProvider config={config}>
+      <Theme name={colorScheme === "dark" ? "light" : "light"}>
+        <StatusBar backgroundColor="dodgerblue" />
+        <Slot />
+      </Theme>
+      </TamaguiProvider>
     </SessionProvider>
   );
 }
